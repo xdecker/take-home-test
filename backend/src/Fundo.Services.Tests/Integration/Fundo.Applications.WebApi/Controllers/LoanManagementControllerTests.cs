276f340e449167150ab956290a9a21492e11894b
xdecker <xavier_deckza@hotmail.com>
2026-01-06 16:06:45 -0500

    feat: add new integration test

--- backend/src/Fundo.Services.Tests/Integration/Fundo.Applications.WebApi/Controllers/LoanManagementControllerTests.cs
+++ backend/src/Fundo.Services.Tests/Integration/Fundo.Applications.WebApi/Controllers/LoanManagementControllerTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -23,6 +25,18 @@ namespace Fundo.Services.Tests.Integration
             var response = await _client.GetAsync("/loan");
 
             Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreateLoan_ShouldReturn_Created()
+        {
+            var response = await _client.PostAsJsonAsync("/loan", new
+            {
+                applicantName = "Integration Test Xavier",
+                amount = 100
+            });
+
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
         }
     }
 }
