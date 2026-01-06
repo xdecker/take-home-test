f393e707f59b6c946f017632383b94a45fff066c
xdecker <xavier_deckza@hotmail.com>
2026-01-06 14:35:48 -0500

    feat: middleware to exceptions
    General middleware to transform exceptions to httpstatus code errors

---
+++ backend/src/Fundo.Applications.WebApi/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Fundo.Applications.WebApi.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex) 
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var statusCode = exception switch
+            {
+                ArgumentException => HttpStatusCode.BadRequest,
+                InvalidOperationException => HttpStatusCode.Conflict,
+                KeyNotFoundException => HttpStatusCode.NotFound,
+                _ => HttpStatusCode.InternalServerError
+            };
+
+            var response = new
+            {
+                error = exception.Message
+            };
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)statusCode;
+
+            return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+    }
+}
